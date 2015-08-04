701f06b87c1fe359fb6521003dcd8a9b14800dbf
dozoisch <hugo.dozois@gmail.com>
2015-08-03 23:44:13 -0700

    [fixed] issue with test with the new context options
    - options is an array of array, so tests are now passing that also

--- test/rules/import.Spec.js
+++ test/rules/import.Spec.js
@@ -8,44 +8,44 @@ tester.run('src/rules/import', rule, {
     {
       code: 'import "something"',
       parser: 'babel-eslint',
-      options: ['lodash']
+      options: [['lodash']]
     },
     {
       code: 'import something from "something"',
       parser: 'babel-eslint',
-      options: ['lodash']
+      options: [['lodash']]
     },
     {
       code: 'import lodash from "something"',
       parser: 'babel-eslint',
-      options: ['lodash']
+      options: [['lodash']]
     },
     {
       code: 'import { something } from "something"',
       parser: 'babel-eslint',
-      options: ['lodash']
+      options: [['lodash']]
     },
     {
       code: 'import { default as other } from "something"',
       parser: 'babel-eslint',
-      options: ['lodash']
+      options: [['lodash']]
     },
     {
       code: 'import find from "lodash/collection/find"',
       parser: 'babel-eslint',
-      options: ['lodash']
+      options: [['lodash']]
     },
     {
       code: 'import find from "react-bootstrap/lib/component"',
       parser: 'babel-eslint',
-      options: ['react-bootstrap', 'lodash']
+      options: [['react-bootstrap', 'lodash']]
     }
   ],
   invalid: [
     {
       code: 'import "lodash"',
       parser: 'babel-eslint',
-      options: ['lodash'],
+      options: [['lodash']],
       errors: [{
         message: 'Importing the entire lodash library is not permitted, please import the specific functions you need'
       }]
@@ -53,7 +53,7 @@ tester.run('src/rules/import', rule, {
     {
       code: 'import _ from "lodash"',
       parser: 'babel-eslint',
-      options: ['lodash'],
+      options: [['lodash']],
       errors: [{
         message: 'Importing the entire lodash library is not permitted, please import the specific functions you need'
       }]
@@ -61,7 +61,7 @@ tester.run('src/rules/import', rule, {
     {
       code: 'import lodash from "lodash"',
       parser: 'babel-eslint',
-      options: ['lodash'],
+      options: [['lodash']],
       errors: [{
         message: 'Importing the entire lodash library is not permitted, please import the specific functions you need'
       }]
@@ -69,7 +69,7 @@ tester.run('src/rules/import', rule, {
     {
       code: 'import { something } from "lodash"',
       parser: 'babel-eslint',
-      options: ['lodash'],
+      options: [['lodash']],
       errors: [{
         message: 'Importing the entire lodash library is not permitted, please import the specific functions you need'
       }]
@@ -77,7 +77,7 @@ tester.run('src/rules/import', rule, {
     {
       code: 'import { default as other } from "lodash"',
       parser: 'babel-eslint',
-      options: ['lodash'],
+      options: [['lodash']],
       errors: [{
         message: 'Importing the entire lodash library is not permitted, please import the specific functions you need'
       }]
@@ -85,7 +85,7 @@ tester.run('src/rules/import', rule, {
     {
       code: 'import rb from "react-bootstrap"',
       parser: 'babel-eslint',
-      options: ['lodash', 'react-bootstrap'],
+      options: [['lodash', 'react-bootstrap']],
       errors: [{
         message: 'Importing the entire react-bootstrap library is not permitted, please import the specific functions you need'
       }]
